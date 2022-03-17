06e827f72efa6ff78b3468c58344b887b2557e15
Abdallah Ziad <boodyziad1@gmail.com>
2022-03-17 17:31:51 +0200

    Add identifiers for other versions.

---
+++ versions.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "3.14.0": {
+    commandStartIdentifier: "Found handler",
+    responseIdentifier: "Decoding response"
+  },
+  "3.141.59": {
+    commandStartIdentifier: "Found handler",
+    responseIdentifier: "To downstream"
+  },
+  "3.11.0": {
+    commandStartIdentifier: "Found handler",
+    responseIdentifier: "Decoding response"
+  },
+  "2.53.0": {
+    commandStartIdentifier: "Handling: ",
+    responseIdentifier: "Finished: "
+  }
+};
