9575c7ebca1b7f7b18a1f89aea773048a0804d9e
Abdallah Ziad <boodyziad1@gmail.com>
2022-03-17 17:31:21 +0200

    Combine all previous functions in the API.

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ if (!(args.length == 2)) {
   console.log("USAGE: node index.js {PATH} {VERSION}");
   process.exit(1);
 }
+
+async function logUniques(path, version) {
+  const Reader = require("./reader");
+  let reader = new Reader(path, version);
+  let uniques = await reader.read();
+  console.log(uniques);
+}
+
+logUniques(...args);
